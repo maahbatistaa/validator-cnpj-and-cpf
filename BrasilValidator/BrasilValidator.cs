238a9394497ef058447815ed820e18d303a820ca
maahbatistaa <104997176+maahbatistaa@users.noreply.github.com>
2022-11-03 12:00:52 -0300

    project base

---
+++ BrasilValidator/BrasilValidator.cs
@@ -0,0 +1,187 @@
+﻿namespace BrasilValidator
+{
+    internal class BrasilValidator
+    {
+        /// <summary>
+        /// Verifica se o CPF é válido
+        /// </summary>
+        /// <param name="cpf">Digitos de um cpf</param>
+        /// <returns>true of false</returns>
+        public static bool EhCpfValido(string cpf )
+        {
+            int soma = 0;
+            int resultado = 0;
+            
+            if (string.IsNullOrWhiteSpace(cpf))
+            {
+                return false;
+            }
+
+            //remove caracteres indesejados
+            cpf = cpf.Replace(".", "")
+                     .Replace("-", "")
+                     .Replace(",", "")
+                     .Replace(" ", "")
+                     .Trim(); 
+
+            if (cpf.Length != 11)
+            {
+                return false;
+            }
+
+            //Confere se não são caracteres iguais
+            else if (cpf[0] == cpf[1] && cpf[1] == cpf[2] && cpf[2] == cpf[3] && 
+                     cpf[3] == cpf[4] && cpf[4] == cpf[5] && cpf[5] == cpf[6] &&
+                     cpf[6] == cpf[7] && cpf[7] == cpf[8] && cpf[8] == cpf[9])
+            {
+                return false;
+            }
+            else
+            {
+                int[] multi1 = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+                int[] multi2 = { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+                //Faz o calculo do primeiro digito
+                for (int i = 0; i < 9; i++)
+                {
+                    soma += int.Parse(cpf[i].ToString()) * multi1[i];
+                }
+
+                resultado = (soma * 10) % 11;
+
+                //Verifica se o resto da divisão é igual a 10, e retorna 0
+                if (resultado == 10) { resultado = 0; }
+
+                //Faz a comparação do resto da divisão com o primeiro digito
+                if (Convert.ToString(resultado) == Convert.ToString(cpf[9]))
+                {
+                    soma = 0;
+
+                    //faz o calculo do segundo digito
+                    for (int i = 0; i < 10; i++)
+                    {
+                        soma += int.Parse(cpf[i].ToString()) * multi2[i];
+                    }
+                    resultado = (soma * 10) % 11;
+
+                    //Verifica se o resto da divisão é igual a 10, e retorna 0
+                    if (resultado == 10) { resultado = 0; }
+
+                    //Faz a comparação do resultado com o segundo digito
+                    if (Convert.ToString(resultado) == Convert.ToString(cpf[10])) 
+                    {
+                       return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Verifica se o CNPJ é válido
+        /// </summary>
+        /// <param name="cnpj">Digitos de um cnpj</param>
+        /// <returns>true or false</returns>
+        public static bool EhCnpjValido(string cnpj)
+        {
+            int soma = 0;
+            int resultado = 0;
+            int[] multi1 = { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multi2 = { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            if (string.IsNullOrWhiteSpace(cnpj))
+            {
+                return false;
+            }
+
+            //remove caracteres indesejados 
+            cnpj = cnpj.Replace(".", "")
+                       .Replace("-", "")
+                       .Replace(",", "")
+                       .Replace(" ", "")
+                       .Replace("/", "")
+                       .Trim();
+
+            //Verifica se caracteres são iguais 
+            if (cnpj[0] == cnpj[1] && cnpj[1] == cnpj[2] && cnpj[2] == cnpj[3] && 
+                cnpj[3] == cnpj[4] && cnpj[4] == cnpj[5] && cnpj[5] == cnpj[6] &&
+                cnpj[6] == cnpj[7] && cnpj[7] == cnpj[8] && cnpj[8] == cnpj[9] &&
+                cnpj[9] == cnpj[10] && cnpj[10] == cnpj[11] && cnpj[11] == cnpj[12])
+            {
+                return false;
+            }
+
+            else if (cnpj.Length != 14)
+            {
+                return false;
+            }
+
+            else
+            {
+                //Faz o calculo do primeiro digito
+                for (int i = 0; i < 12; i++)
+                {
+                    soma += int.Parse(cnpj[i].ToString()) * multi1[i];
+                }
+
+                resultado = soma % 11;
+
+                //Verifica se o resto da divisão é 1 ou 0, e retorna 0
+                if (resultado == 0 || resultado == 1)
+                {
+                    resultado = 0;
+                }
+                else
+                {
+                    resultado = 11 - resultado;
+                }
+
+                //Faz a comparação do resto da divisão com o primeiro digito
+                if (Convert.ToString(resultado) == Convert.ToString(cnpj[12]))
+                {
+                    soma = 0;
+
+                    //faz o calculo do segundo digito
+                    for (int i = 0; i < 13; i++)
+                    {
+                        soma += int.Parse(cnpj[i].ToString()) * multi2[i];
+                    }
+                    resultado = soma % 11;
+
+                    //Verifica se o resto da divisão é 1 ou 0, e retorna 0
+                    if (resultado == 0 || resultado == 1)
+                    {
+                        resultado = 0;
+                    }
+                    else
+                    {
+                        resultado = 11 - resultado;
+                    }
+
+                    //Faz a comparação do resultado com o segundo digito
+                    if (Convert.ToString(resultado) == Convert.ToString(cnpj[13]))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+    }
+}
---
+++ BrasilValidator/Program.cs
@@ -0,0 +1,107 @@
+﻿using BrasilValidator;
+
+var testes = new Teste[] {
+    new Teste(null,false),
+    new Teste(string.Empty,false),
+    new Teste("           ",false),
+    new Teste("123 45678901",false),
+    new Teste("1234567890",false),
+    new Teste("123456789012",false),
+    new Teste("00000000000",false),
+    new Teste("11111111111",false),
+    new Teste("22222222222",false),
+    new Teste("33333333333",false),
+    new Teste("44444444444",false),
+    new Teste("55555555555",false),
+    new Teste("66666666666",false),
+    new Teste("77777777777",false),
+    new Teste("88888888888",false),
+    new Teste("99999999999",false),
+    new Teste("12433468090",false),
+    new Teste("124334680-94",true),
+    new Teste("12433468094",true),
+    new Teste("12433468094 ",true),
+    new Teste(" 12433468094",true),
+    new Teste("124 334 680 94",true),
+    new Teste("124.334.680-94",true),
+    new Teste("449.695.000-61",true),
+    new Teste("888.668.281-65",true),
+};
+
+var testes2 = new Teste[]{
+    new Teste(null,false),
+    new Teste(string.Empty,false),
+    new Teste("           ",false),
+    new Teste("123 45678901234",false),
+    new Teste("12345678901234",false),
+    new Teste("00000000000000",false),
+    new Teste("11111111111111",false),
+    new Teste("22222222222222",false),
+    new Teste("33333333333333",false),
+    new Teste("44444444444444",false),
+    new Teste("55555555555555",false),
+    new Teste("66666666666666",false),
+    new Teste("77777777777777",false),
+    new Teste("88888888888888",false),
+    new Teste("999999999999999",false),
+    new Teste("76 268 126/0001-03",true),
+    new Teste("76.268.126/0001-03",true),
+    new Teste("76 268 126 0001 03",true),
+    new Teste("762681260001-03",true),
+    new Teste(" 76268126000103",true),
+    new Teste("76268126000103 ",true),
+    new Teste("76268126000103",true),
+    new Teste("98.750.901/0001-03",true),
+    new Teste("25.437.016/000197",true),
+    new Teste("53.662.955/0001-70",true),
+    new Teste("00.000.000/0001-91",true),
+};
+
+Console.WriteLine("Testando CPF:");
+
+foreach (var teste in testes)
+{
+    Console.Write($"  {teste.TextoATestar,-16} : ");
+
+    try
+    {
+        var resultado = BrasilValidator.BrasilValidator5.EhCpfValido(teste.TextoATestar);
+        var passorfail = resultado == teste.ResultadoEsperado ? "PASS" : "FAIL";
+        Console.WriteLine($"{resultado,-6} : {passorfail}");
+    }
+    catch
+    {
+        Console.WriteLine($"[erro] : FAIL");
+    }
+}
+
+Console.WriteLine("\n\nTestando CNPJ:");
+
+foreach (var teste in testes2)
+{
+    Console.Write($"  {teste.TextoATestar,-16} : ");
+
+    try
+    {
+        var resultado = BrasilValidator.BrasilValidator5.EhCnpjValido(teste.TextoATestar);
+        var passorfail = resultado == teste.ResultadoEsperado ? "PASS" : "FAIL";
+        Console.WriteLine($"{resultado,-6} : {passorfail}");
+    }
+    catch
+    {
+        Console.WriteLine($"[erro] : FAIL");
+    }
+}
+
+
+internal class Teste
+{
+    public Teste(string? textoATestar, bool resultadoEsperado)
+    {
+        TextoATestar = textoATestar;
+        ResultadoEsperado = resultadoEsperado;
+    }
+
+    public string? TextoATestar { get; set; }
+    public bool ResultadoEsperado { get; set; }
+}
